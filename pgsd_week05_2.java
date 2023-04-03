ba8b49d00240a412abbaa85c80c82bf66549d0ba
ByeongGyu Kang <kbk7525@naver.com>
2023-04-03 22:44:42 +0900

    프로그래밍스튜디오 같은 숫자는 싫어

---
+++ pgsd_week05_2.java
@@ -0,0 +1,33 @@
+public class pgsd_week05_2 {
+    public static void main(String[] args) {
+        ps05_2_Solution s = new ps05_2_Solution();
+        int[] arr = {4,4,4,3,3};
+        System.out.println(s.solution(arr));
+    }
+}
+class ps05_2_Solution {
+    public int[] solution(int []arr) {
+        int idx = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(i == 0) {
+                idx++;
+            }
+            else if(arr[i] != arr[i-1]) {
+                idx++;
+            }
+        }
+        int[] answer = new int[idx];
+        idx = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(i == 0) {
+                answer[idx] = arr[0];
+                idx++;
+            }
+            else if(arr[i] != arr[i-1]) {
+                answer[idx] = arr[i];
+                idx++;
+            }
+        }
+        return answer;
+    }
+}

dcfd9c97a317ea712ef6c66ca5b49223f91cd220
ByeongGyu Kang <kbk7525@naver.com>
2023-12-22 16:38:13 +0900

    2164 카드2

---
+++ 백준/카드2.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 카드2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        int n = scanner.nextInt();
+        for(int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        while(queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}

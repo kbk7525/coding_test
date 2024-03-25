a53bfc5d714bc49622362624c3932e31e5f81c4c
ByeongGyu Kang <kbk7525@naver.com>
2024-03-25 13:40:33 +0900

    예상 대진표

---
+++ programmers/예상대진표.java
@@ -0,0 +1,16 @@
+public class 예상대진표 {
+}
+class Solution7 {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+        while(true) {
+            a = a/2+a%2;
+            b = b/2+b%2;
+            answer++;
+            if(a == b) {
+                break;
+            }
+        }
+        return answer;
+    }
+}

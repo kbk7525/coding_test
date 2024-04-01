571718fc1280fbc0e1381d3bec197f96bb1007ae
ByeongGyu Kang <kbk7525@naver.com>
2024-04-01 15:32:43 +0900

    16212 정열적인 정렬

---
+++ 백준/정열적인정렬.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 정열적인정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i : arr) {
+            bw.write(String.valueOf(i));
+            bw.write(" ");
+        }
+        bw.flush();
+    }
+}
